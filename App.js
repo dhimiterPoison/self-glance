cd89016dfb541028d6ae2b72b9690c3608f7dbdf
dhimiterPoison <dhimiter.helmi@gmail.com>
2024-01-27 13:46:29 +0100

    initialized repo

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+// import { GeistSans } from 'geist/font/sans';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{
+        fontSize: 30
+      }}>Self Glance</Text>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -16,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    // fontFamily: GeistSans
   },
 });
